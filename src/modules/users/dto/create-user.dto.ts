116ed01fc93a1c5bbecdccaed00451803aaf3004
Olivio Freitas <ojfneto92@gmail.com>
2023-07-18 10:14:55 -0300

    feat: add validators to user

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,6 +1,17 @@
+import { IsNotEmpty, IsEmail, IsString, MinLength } from 'class-validator';
+
 //data transfer object
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }
