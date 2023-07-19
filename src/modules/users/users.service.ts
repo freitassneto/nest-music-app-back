7ac9d254717c6841fdb8d51aba6ca7b17bb44a42
Olivio Freitas <ojfneto92@gmail.com>
2023-07-19 12:11:41 -0300

    feat: implement database with prisma

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -35,8 +35,16 @@ export class UsersService {
     return user;
   }
 
+  async findByEmail(email: string) {
+    const user = await this.usersRepository.findOne(email);
+    return user;
+  }
+
   async update(id: string, updateUserDto: UpdateUserDto) {
     const user = await this.usersRepository.update(id, updateUserDto);
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
     return user;
   }
 
