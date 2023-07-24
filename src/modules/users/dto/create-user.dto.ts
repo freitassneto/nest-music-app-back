e169294b76222438cbf644b54fa8c0036ddd6fa4
Olivio Freitas <ojfneto92@gmail.com>
2023-07-24 11:46:04 -0300

    feat: implements swagger documentation

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,17 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { hashSync } from 'bcryptjs';
 import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsEmail, IsString, MinLength } from 'class-validator';
 
 //data transfer object
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Nome do Usuário',
+    type: String,
+    default: 'User',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Email do Usuário',
+    type: String,
+    default: 'user@mail.com',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'Senha do Usuário',
+    type: String,
+    default: 'password',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
